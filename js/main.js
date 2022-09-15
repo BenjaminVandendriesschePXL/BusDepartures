15baa921460bb576c8c2241221389cc9e03eea47
BenjaminVandendriesschePXL <benjamin.vandendriessche@student.pxl.be>
2022-09-15 14:49:30 +0200

    Added trainstation

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ let configData;
 const dataGatheringInterval = setInterval(callApi, 14400000 );
 const removePastDeparturesInterval = setInterval(removeOldDepartures,60000)
 let busStopName
+let trainStopName
 
 function removeOldDepartures() {
-    let dataSet = document.getElementById("bus_departures")
+    let dataSet = document.getElementById("Bus_departures")
     let children = dataSet.getElementsByTagName("tr");
     let currentTime = new Date();
     console.log(currentTime)
@@ -15,7 +16,21 @@ function removeOldDepartures() {
             console.log("delete")
             if(children[i].hasChildNodes()){
                 children[i].removeChild(children[i].children[0])
-            };
+            }
+            dataSet.removeChild(children[i]);
+        }
+        console.log("negative")
+    }
+    dataSet = document.getElementById("Train_departures")
+    children = dataSet.getElementsByTagName("tr");
+
+    for (let i = 0; i<children.length;i++){
+        let time = new Date(children[i].getAttribute("departure"))
+        if(time - currentTime < 0){
+            console.log("delete")
+            if(children[i].hasChildNodes()){
+                children[i].removeChild(children[i].children[0])
+            }
             dataSet.removeChild(children[i]);
         }
         console.log("negative")
@@ -31,6 +46,8 @@ if(navigator.geolocation){
 
 function loadConfig(position) {
     configData = getConfigData(position);
+    busStopName = configData.BusStopName;
+    trainStopName = configData.TrainStopName;
     callApi();
 }
 
@@ -39,67 +56,80 @@ function callApi() {
     console.log("GetData");
     fetch(`https://transit.hereapi.com/v8/departures?ids=${configData.busStopIds}&maxPerBoard=50&apikey=${api_key}`)
         .then((response)=>{ return response.json()})
-        .then((data)=>prepareData(data.boards));
+        .then((data)=>prepareData((data.boards),"Bus"));
+    console.log("GetData");
+    fetch(`https://transit.hereapi.com/v8/departures?ids=${configData.trainStopIds}&maxPerBoard=50&apikey=${api_key}`)
+        .then((response)=>{ return response.json()})
+        .then((data)=>prepareData((data.boards),"Train"));
+
+
 }
 
-function prepareData(data){
-    busStopName = data[0].place.name
-    let busDepartures = [];
+function prepareData(data,transport){
+
+    let departureList = [];
     console.log(data);
-    let list = document.getElementById("bus_departures");
+    let list = document.getElementById(transport+"_departures");
     for(let i = 0;i<data.length;i++){
         for(let j = 0;j<data[i].departures.length;j++){
-            busDepartures.push([data[i].departures[j],"Busstop: "+ data[i].place.code]);
+            departureList.push([data[i].departures[j],transport+"stop: "+ data[i].place.code]);
         }
     }
-    busDepartures.sort(function(a,b){
+    departureList.sort(function(a,b){
         a[0].time = new Date(a[0].time);
         b[0].time = new Date(b[0].time);
         return a[0].time - b[0].time;
     })
-    createTable(busDepartures);
+    createTable(departureList,transport);
 
 }
 
 
 
 
-function createTable(busDepartures) {
-    let table = document.getElementById("bus_departures");
+function createTable(departureList,transport) {
+    let tableId = transport+"_departures"
+    console.log(tableId)
+    let table = document.getElementById(tableId);
     table.innerHTML="";
-    let busStopNameHeader = document.getElementById("busStopName")
-    let headerText = document.createTextNode(busStopName)
-    busStopNameHeader.appendChild(headerText)
-    for (let i = 0; i < busDepartures.length; i++)
+    let trainStopNameHeader = document.getElementById(transport+"StopName")
+    let headerText
+    if(transport === "Bus") {
+        headerText = document.createTextNode(busStopName)
+    }else if(transport === "Train") {
+        headerText = document.createTextNode(trainStopName)
+    }
+    trainStopNameHeader.appendChild(headerText)
+    for (let i = 0; i < departureList.length; i++)
     {
-        console.log(busDepartures[i]);
+        console.log(departureList[i]);
         let newRow = document.createElement("tr");
         let secondRow = document.createElement("tr");
         let busNumberCell = document.createElement("td");
-        let busNumberText = document.createTextNode(busDepartures[i][0].transport.name);
+        let busNumberText = document.createTextNode(departureList[i][0].transport.name);
         let busNumberSpan = document.createElement("span")
         busNumberSpan.appendChild(busNumberText)
         let busDepartureTimeCell = document.createElement("td");
-        let busDepartureTimeText = document.createTextNode(busDepartures[i][0].time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}));
-        if(busDepartures[i][0].delay > 0 ){
+        let busDepartureTimeText = document.createTextNode(departureList[i][0].time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}));
+        if(departureList[i][0].delay > 0 ){
             busDepartureTimeCell.style.color = "red";
         }
         let busDestinationCell = document.createElement("td");
-        let busDestinationText = document.createTextNode(busDepartures[i][0].transport.headsign);
+        let busDestinationText = document.createTextNode(departureList[i][0].transport.headsign);
         let linebreak = document.createElement("br");
 
         busNumberCell.appendChild(busNumberSpan);
-        busNumberSpan.style.color = busDepartures[i][0].transport.textColor;
+        busNumberSpan.style.color = departureList[i][0].transport.textColor;
         //busNumberCell.setAttribute("rowspan",2)
         busNumberCell.classList.add("busNumber","spacingUnder")
-        busNumberSpan.style.backgroundColor = busDepartures[i][0].transport.color;
+        busNumberSpan.style.backgroundColor = departureList[i][0].transport.color;
         busDepartureTimeCell.appendChild(busDepartureTimeText);
         busDepartureTimeCell.appendChild(linebreak);
         busDepartureTimeCell.appendChild(busDestinationText);
         busDepartureTimeCell.classList.add("spacingUnder")
 
-        newRow.setAttribute("departure",busDepartures[i][0].time);
-        secondRow.setAttribute("departure",busDepartures[i][0].time);
+        newRow.setAttribute("departure",departureList[i][0].time);
+        secondRow.setAttribute("departure",departureList[i][0].time);
 
         newRow.appendChild(busNumberCell);
         newRow.appendChild(busDepartureTimeCell);
