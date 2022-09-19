a0712ec9b6cf3d16f69c9ec45bfeea1c9373a8d4
Benjamin Vandendriessche <benjamin.vandendriessche@student.pxl.be>
2022-09-19 15:59:41 +0200

    first cards version

--- js/main.js
+++ js/main.js
@@ -88,38 +88,82 @@ function departures(){
     }
 
 
+    function createCard(name,time,headsign,textColor,color){
+       
+        let card = document.createElement("div")
+        let transportNumber = document.createElement("div")
+        let cardBody = document.createElement("div")
+        let cardTime = document.createElement("p")
+        let cardLine = document.createElement("p")
+        let transportNumberText = document.createTextNode(name)
+        let transportTime = document.createTextNode(time)
+        let transportHeadsign = document.createTextNode(headsign)
+        
+        card.classList.add("card","h-1","flex-row")
+        transportNumber.classList.add("card-img-left", "busNumber")
+        cardBody.classList.add("card-body")
+        cardTime.classList.add("card-text")
+        cardLine.classList.add("card-text")
 
+        transportNumber.style.color = textColor;
+        transportNumber.style.backgroundColor = color;
+        cardTime.style.margin = 0;
+
+        card.appendChild(transportNumber)
+        card.appendChild(cardBody)
+        cardBody.appendChild(cardTime)
+        cardBody.appendChild(cardLine)
+        transportNumber.appendChild(transportNumberText)
+        cardTime.appendChild(transportTime)
+        cardLine.appendChild(transportHeadsign)
+        return card
+        // <div className="row">
+        //     <div className="col">
+        //         <div className="card h-1 flex-row">
+        //             <div className="card-img-left"
+        //                  style="width:40px;height:40px;margin:auto;background:red;text-align:center;line-height:40px;border-radius:10px">333
+        //             </div>
+        //             <div className="card-body" style="padding:5px">
+        //                 <p className="card-text" style="margin:0">11u45</p>
+        //                 <p className="card-text">leuven-tremolo</p>
+        //
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <div className="col-sm">
+    }
 
     function createTable(departureList,transport) {
-        let tableId = transport+"_departures"
-        let table = document.getElementById(tableId);
-        table.innerHTML="";
+        let divId = transport+"_departures"
+         let divToFill = document.getElementById(divId);
+        // table.innerHTML="";
         let trainStopNameHeader = document.getElementById(transport+"StopName")
 
         for (let i = 0; i < departureList.length; i++)
         {
-            let newRow = document.createElement("tr");
-            let busNumberCell = document.createElement("td");
-            let busNumberText = document.createTextNode(departureList[i][0].transport.name);
-            let busNumberSpan = document.createElement("span")
-            busNumberSpan.appendChild(busNumberText)
-            let busDepartureTimeCell = document.createElement("td");
-            let busDepartureTimeText = document.createTextNode(departureList[i][0].time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}));
-
-            let busDestinationText = document.createTextNode(departureList[i][0].transport.headsign);
-            let linebreak = document.createElement("br");
-
-            busNumberCell.appendChild(busNumberSpan);
-            busNumberSpan.style.color = departureList[i][0].transport.textColor;
-            busNumberSpan.style.backgroundColor = departureList[i][0].transport.color;
-            busDepartureTimeCell.appendChild(busDepartureTimeText);
-            busDepartureTimeCell.appendChild(linebreak);
-            busDepartureTimeCell.appendChild(busDestinationText);
-
-            newRow.setAttribute("departure",departureList[i][0].time);
-            newRow.appendChild(busNumberCell);
-            newRow.appendChild(busDepartureTimeCell);
-            table.appendChild(newRow);
+            divToFill.appendChild(createCard(departureList[i][0].transport.name,departureList[i][0].time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}),departureList[i][0].transport.headsign,departureList[i][0].transport.textColor,departureList[i][0].transport.color))
+            // let newRow = document.createElement("tr");
+            // let busNumberCell = document.createElement("td");
+            // let busNumberText = document.createTextNode(departureList[i][0].transport.name,);
+            // let busNumberSpan = document.createElement("span")
+            // busNumberSpan.appendChild(busNumberText)
+            // let busDepartureTimeCell = document.createElement("td");
+            // let busDepartureTimeText = document.createTextNode(departureList[i][0].time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'}));
+            //
+            // let busDestinationText = document.createTextNode(departureList[i][0].transport.headsign);
+            // let linebreak = document.createElement("br");
+            //
+            // busNumberCell.appendChild(busNumberSpan);
+            // busNumberSpan.style.color = departureList[i][0].transport.textColor;
+            // busNumberSpan.style.backgroundColor = departureList[i][0].transport.color;
+            // busDepartureTimeCell.appendChild(busDepartureTimeText);
+            // busDepartureTimeCell.appendChild(linebreak);
+            // busDepartureTimeCell.appendChild(busDestinationText);
+            //
+            // newRow.setAttribute("departure",departureList[i][0].time);
+            // newRow.appendChild(busNumberCell);
+            // newRow.appendChild(busDepartureTimeCell);
+            // table.appendChild(newRow);
         }
     }
 }
